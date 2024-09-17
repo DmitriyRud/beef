5e96ad66a7303ee1e019559606a6adada8d92aa6
Dmitriy Rudakov <cska2004@gmail.com>
2024-09-17 10:22:04 +0200

    slide text updated4

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -64,9 +64,8 @@ module.exports = function Home({ categories }) {
             </div>
             <div className="swiper-slide slide2">
               <div className="slide2-container">
-                <div className="slide2-item">
-                  100% качество от производителя
-                </div>
+                <div className="slide2-item">100% качество</div>
+                <div className="slide2-item">от производителя</div>
               </div>
               <div className="image-slider">
                 <img
