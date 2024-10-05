6f139287a7b93db3c58306070a109b0971b9ccda
Dmitriy Rudakov <cska2004@gmail.com>
2024-10-05 13:16:34 +0200

    sitemap & yandex verification added

--- src/routers/adminPanelRouter.js
+++ src/routers/adminPanelRouter.js
@@ -1,13 +1,13 @@
-const router = require('express').Router();
-const bcrypt = require('bcrypt');
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
 
-const renderTemplate = require('../lib/renderTemplate');
-const AdminPanel = require('../views/AdminPanel');
-const { User } = require('../../db/models');
-const { Category } = require('../../db/models');
-const { isNoAuth } = require('../middlewares/isAuth');
+const renderTemplate = require("../lib/renderTemplate");
+const AdminPanel = require("../views/AdminPanel");
+const { User } = require("../../db/models");
+const { Category } = require("../../db/models");
+const { isNoAuth } = require("../middlewares/isAuth");
 
-router.get('/', isNoAuth, async (req, res) => {
+router.get("/", isNoAuth, async (req, res) => {
   try {
     const categories = await Category.findAll({ raw: true });
     renderTemplate(AdminPanel, { categories }, res);
@@ -16,7 +16,7 @@ router.get('/', isNoAuth, async (req, res) => {
   }
 });
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.findOne({ where: { email } });
@@ -26,27 +26,27 @@ router.post('/', async (req, res) => {
         req.session.email = user.email;
 
         req.session.save(() => {
-          res.json({ msg: 'Успешный вход' });
+          res.json({ msg: "Успешный вход" });
         });
       } else {
-        res.json({ err: 'Пароль неверный' });
+        res.json({ err: "Пароль неверный" });
       }
     } else {
-      res.json({ err: 'Такой пользователь не найден' });
+      res.json({ err: "Такой пользователь не найден" });
     }
   } catch (error) {
-    res.send('Что-то пошло не так', error);
+    res.send("Что-то пошло не так", error);
   }
 });
 
-router.get('/logout', (req, res) => {
+router.get("/logout", (req, res) => {
   req.session.destroy((error) => {
     if (error) {
       console.log(error);
     } else {
       res.clearCookie(process.env.COOKIE_NAME);
     }
-    res.redirect('/');
+    res.redirect("/");
   });
 });
 
--- src/routers/modalRouter.js
+++ src/routers/modalRouter.js
@@ -1,11 +1,11 @@
-const modalRouter = require('express').Router();
-const nodemailer = require('nodemailer');
+const modalRouter = require("express").Router();
+const nodemailer = require("nodemailer");
 
-const { priceRequest, ManagerEmail } = require('../../db/models');
+const { priceRequest, ManagerEmail } = require("../../db/models");
 
 const transporter = nodemailer.createTransport({
   port: 465,
-  host: 'smtp.gmail.com',
+  host: "smtp.gmail.com",
   // service: 'gmail',
   auth: {
     user: process.env.EMAIL,
@@ -14,27 +14,27 @@ const transporter = nodemailer.createTransport({
   secure: true, // upgrades later with STARTTLS -- change this based on the PORT
 });
 
-modalRouter.post('/', async (req, res) => {
+modalRouter.post("/", async (req, res) => {
   const usersMailObject = await ManagerEmail.findAll({
-    attributes: ['email'],
+    attributes: ["email"],
     raw: true,
   });
   const usersMail = usersMailObject.map((el) => el.email);
   const { name, phone, email } = req.body;
   const mailData = {
-    from: 'chipolinaryi@gmail.com',
+    from: "chipolinaryi@gmail.com",
     to: usersMail,
-    subject: 'Запрос прайса',
-    text: ' ',
+    subject: "Запрос прайса Premium Beef",
+    text: " ",
 
-    html: `<b> Посетитель сайта ${name} запросил актуальные цены. </b><br> Информацию ему необходимо выслать на почту ${email}. Для уточнения данных пользователь оставил номер телефона: ${phone} <br/> `,
+    html: `<b> Посетитель сайта https://premiumbeef.ru/ указал имя: ${name} и запросил актуальные цены. </b><br> Информацию ему необходимо выслать на почту ${email}. Для уточнения данных пользователь оставил номер телефона: ${phone} <br/> `,
   };
 
   transporter.sendMail(mailData, (error, info) => {
     if (error) {
       return console.log(error);
     }
-    res.status(200).send({ message: 'Mail send', message_id: info.messageId });
+    res.status(200).send({ message: "Mail send", message_id: info.messageId });
   });
 
   try {
